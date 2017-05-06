f1d97edf0514ccc75b681ebc2d969f0481b16499
cuizhennan <1378496183@qq.com>
2017-05-06 19:41:39 +0800

    update logback

--- springdemo/src/main/java/xyz/monkeycoding/servers/DefaultController.java
+++ springdemo/src/main/java/xyz/monkeycoding/servers/DefaultController.java
@@ -1,5 +1,7 @@
 package xyz.monkeycoding.servers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,10 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class DefaultController {
+    private static final Logger logger = LoggerFactory.getLogger(DefaultController.class.getName());
 
     @RequestMapping("/")
     @ResponseBody
     public String check() {
+        logger.info("server check: success");
         return "SUCCESS";
     }
 }
